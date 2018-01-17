9e8d3a17706e8d30fae7582c4a5cdc51589eeae0
San Kang <san0321@gmail.com>
2018-01-16 23:32:22 -0800

    Updated Mobile version

--- src/template.js
+++ src/template.js
@@ -3,6 +3,7 @@ export default ({ body, title, initialState }) => {
     <!DOCTYPE html>
     <html>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <script>window.__APP_INITIAL_STATE__ = ${initialState}</script>
         <title>${title}</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
